2e2509c586cde9d267b9386018476a93af32b868
Ludovic Vannoorenberghe <ludo@datacamp.com>
2021-02-23 16:40:51 +0000

    add terratest suite

--- examples/main.tf
+++ examples/main.tf
@@ -3,9 +3,23 @@ provider "kuma" {
   api_token = "test123"
 }
 
+resource "kuma_traffic_permission" "yolo_permission" {
+  mesh = "default"
+  name = "yolo_permission"
 
-    }
-
+  sources {
+    match = {
+      "kuma.io/service" = "*"
     }
   }
+
+  destinations {
+    match = {
+      "kuma.io/service" = "*"
+    }
+  }
+}
+
+output "yolo_permission_name" {
+  value = kuma_traffic_permission.yolo_permission.name
 }
