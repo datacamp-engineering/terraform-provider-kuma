9908927022e088c1747dbe843f60313ef34b1309
Dean Terweduwe <dean.terweduwe@student.ucll.be>
2021-03-17 17:37:37 +0100

    Added flatten and create

--- kuma/resource-retry-policy.go
+++ kuma/resource-retry-policy.go
@@ -160,18 +160,18 @@ func resourceRetry() *schema.Resource {
 func resourceRetryCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
 	store := m.(core_store.ResourceStore)
 
-	permission := createKumaRetryFromResourceData(d)
+	retry := createKumaRetryFromResourceData(d)
 
 	meshName := readStringFromResource(d, "mesh")
 	name := readStringFromResource(d, "name")
 
-	err := store.Create(ctx, &permission, core_store.CreateByKey(name, meshName))
+	err := store.Create(ctx, &retry, core_store.CreateByKey(name, meshName))
 
 	if err != nil {
 		return diag.FromErr(err)
 	}
 
-	d.SetId(permission.Meta.GetName())
+	d.SetId(retry.Meta.GetName())
 	return resourceRetryRead(ctx, d, m)
 }
 
@@ -183,31 +183,35 @@ func resourceRetryRead(ctx context.Context, d *schema.ResourceData, m interface{
 	name := d.Id()
 	meshName := d.Get("mesh").(string)
 
-	permission := &mesh.RetryResource{
+	retry := &mesh.RetryResource{
 		Spec: &mesh_proto.Retry{},
 	}
 
-	err := store.Get(ctx, permission, core_store.GetByKey(name, meshName))
+	err := store.Get(ctx, retry, core_store.GetByKey(name, meshName))
 
 	if err != nil {
 		return diag.FromErr(err)
 	}
 
-	d.SetId(permission.Meta.GetName())
+	d.SetId(retry.Meta.GetName())
 
-	if err := d.Set("name", permission.Meta.GetName()); err != nil {
+	if err := d.Set("name", retry.Meta.GetName()); err != nil {
 		return diag.FromErr(err)
 	}
 
-	if err := d.Set("mesh", permission.Meta.GetMesh()); err != nil {
+	if err := d.Set("mesh", retry.Meta.GetMesh()); err != nil {
 		return diag.FromErr(err)
 	}
 
-	if err := d.Set("sources", flattenKumaSelector(permission.Spec.Sources)); err != nil {
+	if err := d.Set("sources", flattenKumaSelector(retry.Spec.Sources)); err != nil {
 		return diag.FromErr(err)
 	}
 
-	if err := d.Set("destinations", flattenKumaSelector(permission.Spec.Destinations)); err != nil {
+	if err := d.Set("destinations", flattenKumaSelector(retry.Spec.Destinations)); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err := d.Set("conf", flattenKumaRetryConf(retry.Spec.Conf)); err != nil {
 		return diag.FromErr(err)
 	}
 
@@ -215,25 +219,25 @@ func resourceRetryRead(ctx context.Context, d *schema.ResourceData, m interface{
 }
 
 func resourceRetryUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	if d.HasChange("sources") || d.HasChange("destinations") {
+	if d.HasChange("sources") || d.HasChange("destinations") || d.HasChange("conf") {
 		store := m.(core_store.ResourceStore)
 
 		meshName := readStringFromResource(d, "mesh")
 		name := readStringFromResource(d, "name")
-		permission := createKumaRetryFromResourceData(d)
+		retry := createKumaRetryFromResourceData(d)
 
-		oldPermission := mesh.RetryResource{
+		oldRetry := mesh.RetryResource{
 			Spec: &mesh_proto.Retry{},
 		}
 
-		err := store.Get(ctx, &oldPermission, core_store.GetByKey(name, meshName))
+		err := store.Get(ctx, &oldRetry, core_store.GetByKey(name, meshName))
 		if err != nil {
 			return diag.FromErr(err)
 		}
 
-		permission.Meta = oldPermission.Meta
+		retry.Meta = oldRetry.Meta
 
-		err = store.Update(ctx, &permission)
+		err = store.Update(ctx, &retry)
 
 		if err != nil {
 			return diag.FromErr(err)
@@ -251,9 +255,9 @@ func resourceRetryDelete(ctx context.Context, d *schema.ResourceData, m interfac
 	name := d.Id()
 	meshName := d.Get("mesh").(string)
 
-	permission := createKumaRetryFromResourceData(d)
+	retry := createKumaRetryFromResourceData(d)
 
-	err := store.Delete(ctx, &permission, core_store.DeleteByKey(name, meshName))
+	err := store.Delete(ctx, &retry, core_store.DeleteByKey(name, meshName))
 
 	if err != nil {
 		return diag.FromErr(err)
@@ -300,20 +304,211 @@ func createKumaRetryFromResourceData(data *schema.ResourceData) mesh.RetryResour
 
 func createKumaRetryConfFromMap(confMap map[string]interface{}) *mesh_proto.Retry_Conf {
 	conf := &mesh_proto.Retry_Conf{}
-	if httpMap, ok := confMap["http"].(map[string]interface{}); ok {
-		conf.Http = createKumaRetryConfHTTPFromMap(httpMap)
+
+	if http, ok := confMap["http"].(map[string]interface{}); ok {
+		conf.Http = createKumaRetryConfHTTPFromMap(http)
+	}
+
+	if grpc, ok := confMap["grpc"].(map[string]interface{}); ok {
+		conf.Grpc = createKumaRetryConfGRPCFromMap(grpc)
+	}
+
+	if tcp, ok := confMap["tcp"].(map[string]interface{}); ok {
+		conf.Tcp = createKumaRetryConfTCPFromMap(tcp)
 	}
 
 	return conf
-
 }
 
 func createKumaRetryConfHTTPFromMap(httpMap map[string]interface{}) *mesh_proto.Retry_Conf_Http {
 	http := &mesh_proto.Retry_Conf_Http{}
+
 	if numRetries, ok := httpMap["numRetries"].(uint32); ok {
 		http.NumRetries = &wrappers.UInt32Value{Value: numRetries}
 	}
 
+	if interval, ok := httpMap["interval"].(string); ok {
+		duration, _ := readDurationFromString(interval)
+		http.PerTryTimeout = duration
+	}
+
+	if backOffMap, ok := httpMap["backOff"].(map[string]interface{}); ok {
+		http.BackOff = createKumaRetryConfHTTPBackOffFromMap(backOffMap)
+	}
+
+	if retiableStatusCodes, ok := httpMap["expected_statuses"].([]uint32); ok {
+		// uIntArray := []*wrappers.UInt32Value{}
+
+		// for _, data := range retiableStatusCodes {
+		// 	item := wrappers.UInt32Value{
+		// 		Value: data,
+		// 	}
+		// 	uIntArray = append(uIntArray, &item)
+		// }
+
+		http.RetriableStatusCodes = retiableStatusCodes
+	}
+
 	return http
 
+}
+
+func createKumaRetryConfGRPCFromMap(grpcMap map[string]interface{}) *mesh_proto.Retry_Conf_Grpc {
+	grpc := &mesh_proto.Retry_Conf_Grpc{}
+
+	if numRetries, ok := grpcMap["numRetries"].(uint32); ok {
+		grpc.NumRetries = &wrappers.UInt32Value{Value: numRetries}
+	}
+
+	if interval, ok := grpcMap["interval"].(string); ok {
+		duration, _ := readDurationFromString(interval)
+		grpc.PerTryTimeout = duration
+	}
+
+	if backOffMap, ok := grpcMap["backOff"].(map[string]interface{}); ok {
+		grpc.BackOff = createKumaRetryConfHTTPBackOffFromMap(backOffMap)
+	}
+
+	if retiableStatusCodes, ok := grpcMap["expected_statuses"].([]mesh_proto.Retry_Conf_Grpc_RetryOn); ok {
+		grpc.RetryOn = retiableStatusCodes
+	}
+
+	return grpc
+}
+
+func createKumaRetryConfTCPFromMap(tcpMap map[string]interface{}) *mesh_proto.Retry_Conf_Tcp {
+	tcp := &mesh_proto.Retry_Conf_Tcp{}
+
+	if maxConnectAttempts, ok := tcpMap["maxConnectAttempts"].(uint32); ok {
+		tcp.MaxConnectAttempts = maxConnectAttempts
+	}
+
+	return tcp
+}
+
+func createKumaRetryConfHTTPBackOffFromMap(backOffMap map[string]interface{}) *mesh_proto.Retry_Conf_BackOff {
+	backOff := &mesh_proto.Retry_Conf_BackOff{}
+
+	if baseInterval, ok := backOffMap["baseInterval"].(string); ok {
+		duration, _ := readDurationFromString(baseInterval)
+		backOff.BaseInterval = duration
+	}
+
+	if maxInterval, ok := backOffMap["maxInterval"].(string); ok {
+		duration, _ := readDurationFromString(maxInterval)
+		backOff.MaxInterval = duration
+	}
+
+	return backOff
+
+}
+
+func flattenKumaRetryConf(conf *mesh_proto.Retry_Conf) map[string]interface{} {
+	confMap := make(map[string]interface{})
+
+	if conf == nil {
+		return confMap
+	}
+
+	if conf.Http != nil {
+		confMap["http"] = flattenKumaRetryConfHTTP(conf.Http)
+	}
+
+	if conf.Grpc != nil {
+		confMap["grpc"] = flattenKumaRetryConfGRPC(conf.Grpc)
+	}
+
+	if conf.Tcp != nil {
+		confMap["tcp"] = flattenKumaRetryConfTCP(conf.Tcp)
+	}
+
+	return confMap
+}
+
+func flattenKumaRetryConfHTTP(http *mesh_proto.Retry_Conf_Http) map[string]interface{} {
+	httpMap := make(map[string]interface{})
+
+	if http == nil {
+		return httpMap
+	}
+
+	if http.NumRetries != nil {
+		httpMap["numRetries"] = http.NumRetries
+	}
+
+	if http.PerTryTimeout != nil {
+		httpMap["perTryTimeout"] = http.PerTryTimeout.String()
+	}
+
+	if http.BackOff != nil {
+		httpMap["backOff"] = flattenKumaHealthCheckConfBackoff(http.BackOff)
+	}
+
+	if http.RetriableStatusCodes != nil {
+		httpMap["retriableStatusCodes"] = http.RetriableStatusCodes
+
+	}
+
+	return httpMap
+
+}
+
+func flattenKumaRetryConfGRPC(grpc *mesh_proto.Retry_Conf_Grpc) map[string]interface{} {
+	grpcMap := make(map[string]interface{})
+
+	if grpc == nil {
+		return grpcMap
+	}
+
+	if grpc.NumRetries != nil {
+		grpcMap["numRetries"] = grpc.NumRetries
+	}
+
+	if grpc.PerTryTimeout != nil {
+		grpcMap["perTryTimeout"] = grpc.PerTryTimeout.String()
+	}
+
+	if grpc.BackOff != nil {
+		grpcMap["backOff"] = flattenKumaHealthCheckConfBackoff(grpc.BackOff)
+	}
+
+	if grpc.RetryOn != nil {
+		grpcMap["retryOn"] = grpc.RetryOn
+
+	}
+
+	return grpcMap
+
+}
+
+func flattenKumaRetryConfTCP(tcp *mesh_proto.Retry_Conf_Tcp) map[string]interface{} {
+	tcpMap := make(map[string]interface{})
+
+	if tcp == nil {
+		return tcpMap
+	}
+	// is this okey? Uint32 is not nilable
+	if tcp.MaxConnectAttempts != 0 {
+		tcpMap["maxConnectAttempts"] = tcp.MaxConnectAttempts
+	}
+
+	return tcpMap
+}
+
+func flattenKumaHealthCheckConfBackoff(backoff *mesh_proto.Retry_Conf_BackOff) map[string]interface{} {
+	backOffMap := make(map[string]interface{})
+
+	if backoff != nil {
+		return backOffMap
+	}
+
+	if backoff.BaseInterval != nil {
+		backOffMap["baseInterval"] = backoff.BaseInterval
+	}
+
+	if backoff.MaxInterval != nil {
+		backOffMap["maxInterval"] = backoff.MaxInterval
+	}
+
+	return backOffMap
 }
